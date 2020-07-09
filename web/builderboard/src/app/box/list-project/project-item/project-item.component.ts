dabe5da174bc0f4b86385db81153659abdc10285
wermar <marcel.wernisch@outlook.de>
2020-07-09 23:09:40 +0200

    BUILBOA-24: add function to open edit dialog

--- web/builderboard/src/app/box/list-project/project-item/project-item.component.ts
+++ web/builderboard/src/app/box/list-project/project-item/project-item.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Project} from '../../../db/entities/project';
+import {MatDialog} from '@angular/material/dialog';
+import {EditProjectDialogComponent} from '../../edit-project-dialog/edit-project-dialog.component';
 
 @Component({
   selector: 'app-project-item',
@@ -10,9 +12,27 @@ export class ProjectItemComponent implements OnInit {
 
   @Input() project: Project;
 
-  constructor() { }
+  constructor(private matDialog: MatDialog) {
+  }
 
   ngOnInit(): void {
   }
 
+  openEditDialog() {
+    const editProjectDialogRef = this.matDialog.open(EditProjectDialogComponent, {
+      width: 'auto',
+      minWidth: '30vw',
+      data: this.project,
+    });
+
+    editProjectDialogRef.afterClosed().subscribe(result => {
+        console.log('Result of Dialog');
+        console.log(result);
+      }
+    );
+  }
+
+  deleteItem() {
+    // TODO will implement in a later step of implementation
+  }
 }
