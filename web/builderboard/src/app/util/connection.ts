b553bcb7e6ce57a165a9567eb92a76e7b786aac6
wermar <marcel.wernisch@outlook.de>
2020-08-18 20:03:28 +0200

    change build of project with flag '--prod' and modify some files

--- web/builderboard/src/app/util/connection.ts
+++ web/builderboard/src/app/util/connection.ts
@@ -1,6 +1,5 @@
 
 export class Connection {
-  public static localURLConnection = 'http://localhost:9000';
+  public static localURLConnection = 'http://localhost:8080';
   public static prodURLConnection = 'http://localhost:9000';
-
 }
