a7c225e749968866bd37dc1059b085940d1e43c2
wermar <marcel.wernisch@outlook.de>
2020-08-21 16:52:48 +0200

    modify readme file add build script for an easier setup

---
+++ buildImages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# setup local variables
+WORKDIR=`pwd`
+DOCKERBUILD="docker build -t"
+DOCKERFILESERVER="builderboard-server-img ."
+DOCKERFILEWEB="builderboard-web-img ."
+echo "WORKDIR set on $WORKDIR"
+
+# execute build
+echo ""
+echo "switch to server directory"
+cd $WORKDIR/server/buildersBoard
+echo `pwd`
+echo "execute build command of docker"
+command $DOCKERBUILD $DOCKERFILESERVER
+echo ""
+echo "successfully created builderboard-server-img -> creating now web image"
+cd $WORKDIR/web/buildersBoard
+echo `pwd`
+echo "execute build command to build web image"
+command $DOCKERBUILD $DOCKERFILEWEB
