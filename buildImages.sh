ae6be4a956f263b211f43ac8ad460e3799fd45af
Marcel <47454606+WERMAR@users.noreply.github.com>
2020-08-28 12:25:47 +0200

    fix issue in shell script

--- buildImages.sh
+++ buildImages.sh
@@ -10,13 +10,13 @@ echo "WORKDIR set on $WORKDIR"
 # execute build
 echo ""
 echo "switch to server directory"
-cd $WORKDIR/server/builderBoard
+cd $WORKDIR/server/buildersBoard
 echo `pwd`
 echo "execute build command of docker"
 command $DOCKERBUILD $DOCKERFILESERVER
 echo ""
 echo "successfully created builderboard-server-img -> creating now web image"
-cd $WORKDIR/web/buildersBoard
+cd $WORKDIR/web/builderBoard
 echo `pwd`
 echo "execute build command to build web image"
 command $DOCKERBUILD $DOCKERFILEWEB
