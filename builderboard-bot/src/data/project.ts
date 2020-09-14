f141bc835b7cf36b9e6cf0aeb9bc5ccf42a503a2
wermar <marcel.wernisch@outlook.de>
2020-09-14 20:32:16 +0200

    [BUILBOA-22] WERMAR: Inital Setup of Bot-Env, first functions and modified Rest-API Controller of User-Endpoint

---
+++ builderboard-bot/src/data/project.ts
@@ -0,0 +1,29 @@
+export class Project {
+
+    orderNumber: string;
+    projectDescription: string;
+    start: string;
+    end: string;
+    reminder: boolean;
+    startReminder: string;
+    endReminder: string;
+    responsiblePersonName: string;
+
+    constructor(orderNumber: string,
+                projectDescription: string,
+                start: string,
+                end: string,
+                reminder: boolean,
+                startReminder: string,
+                endReminder: string,
+                responsiblePersonName: string) {
+        this.orderNumber = orderNumber;
+        this.projectDescription = projectDescription;
+        this.start = start;
+        this.end = end;
+        this.reminder = reminder;
+        this.startReminder = startReminder;
+        this.endReminder = endReminder;
+        this.responsiblePersonName = responsiblePersonName;
+    }
+}
